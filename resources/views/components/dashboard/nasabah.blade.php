d75fca0b36f22a9007c998e15a229fcee76cec35
Reyonl <liurey55@gmail.com>
2025-03-27 16:45:59 +0700

    perbaiki tampilan dashboard nasabah

--- resources/views/components/dashboard/nasabah.blade.php
+++ resources/views/components/dashboard/nasabah.blade.php
@@ -3,7 +3,10 @@
 
 @section('content')
 <div class="container">
-    <h2>Selamat datang, {{ auth()->user()->nama }}</h2>
-    <p>Ini adalah dashboard Nasabah Anda.</p>
+    <h2 class="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">
+        Selamat datang, {{ auth()->user()->nama }}
+    </h2>
+
+
 </div>
 @endsection
