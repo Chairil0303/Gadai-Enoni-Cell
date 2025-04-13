5c23e8efd13c107f3fc792d0f14d508e34e4e0bc
Reyonl <liurey55@gmail.com>
2025-04-13 14:50:57 +0700

    benerin bagian notif ke wa

--- app/Http/Controllers/NasabahPaymentController.php
+++ app/Http/Controllers/NasabahPaymentController.php
@@ -256,6 +256,8 @@ public function handleNotificationJson(Request $request)
 
         $message = "*📦 Transaksi Tebus Berhasil!*\n\n" .
             "🆔 No BON: {$barang->no_bon}\n" .
+            "🏷 Nama Barang: {$barang->nama_barang}\n" .
+            "🏦 Cabang: {$barang->nasabah->user->cabang->nama_cabang}\n" .
             "👤 Nama: {$nasabah->nama}\n" .
             "💰 Jumlah: Rp " . number_format($grossAmount, 0, ',', '.') . "\n" .
             "📅 Tanggal: " . now()->format('d-m-Y') . "\n\n" .
