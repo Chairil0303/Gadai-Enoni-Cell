7a3e2157d1bf435a734e8d5a086d84739c4b2866
chairil0303 <chairilsyahrain24@gmail.com>
2025-04-24 09:54:43 +0700

    menambahkan nilai crated_at pada seeder cabang

--- database/seeders/CabangSeeder.php
+++ database/seeders/CabangSeeder.php
@@ -12,9 +12,23 @@ class CabangSeeder extends Seeder
         DB::table('cabang')->truncate();
 
         DB::table('cabang')->insert([
-            ['nama_cabang' => 'Cabang Kalisuren', 'alamat' => 'Jl. Kalisuren No.1, Bogor', 'kontak' => '081234567890', 'google_maps_link' => 'https://maps.app.goo.gl/raomLc4mYXNGZPjLA'],
-            ['nama_cabang' => 'Cabang Parung', 'alamat' => 'Jl. Asia Afrika No.2, Parung', 'kontak' => '081234567891','google_maps_link' => 'https://maps.app.goo.gl/raomLc4mYXNGZPjLA'],
-            ['nama_cabang' => 'Cabang Lebak Wangi', 'alamat' => 'Jl. Raya Darmo No.3, Lebak Wangi', 'kontak' => '081234567892','google_maps_link' => 'https://maps.app.goo.gl/raomLc4mYXNGZPjLA'],
+            ['nama_cabang' => 'Cabang Kalisuren', 
+            'alamat' => 'Jl. Kalisuren No.1, Bogor', 
+            'kontak' => '081234567890', 
+            'google_maps_link' => 'https://maps.app.goo.gl/raomLc4mYXNGZPjLA',
+            'created_at' => now()->subDays(rand(1, 30))],
+
+            ['nama_cabang' => 'Cabang Parung', 
+            'alamat' => 'Jl. Asia Afrika No.2, Parung', 
+            'kontak' => '081234567891',
+            'google_maps_link' => 'https://maps.app.goo.gl/raomLc4mYXNGZPjLA',
+            'created_at' => now()->subDays(rand(1, 30))],
+            
+            ['nama_cabang' => 'Cabang Lebak Wangi', 
+            'alamat' => 'Jl. Raya Darmo No.3, Lebak Wangi', 
+            'kontak' => '081234567892',
+            'google_maps_link' => 'https://maps.app.goo.gl/raomLc4mYXNGZPjLA',
+            'created_at' => now()->subDays(rand(1, 30))],
         ]);
     }
 }
